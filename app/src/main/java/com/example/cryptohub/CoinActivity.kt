72172b3697347c8b625348b316a9301028258f6a
Mahdi <MahdiPorkar2002@gmail.com>
2023-02-04 08:24:46 -0800

    coin activity created

---
+++ app/src/main/java/com/example/cryptohub/CoinActivity.kt
@@ -0,0 +1,17 @@
+package com.example.cryptohub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cryptohouse.databinding.ActivityCoinBinding
+
+class CoinActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityCoinBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCoinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
