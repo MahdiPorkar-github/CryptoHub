380836e3e6720937a107ce20caa5247d4e6590d6
Mahdi <MahdiPorkar2002@gmail.com>
2023-02-04 06:31:38 -0800

    toolbar module implemented

--- app/src/main/java/com/example/cryptohub/MarketActivity.kt
+++ app/src/main/java/com/example/cryptohub/MarketActivity.kt
@@ -3,16 +3,16 @@ package com.example.cryptohub
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.example.cryptohub.databinding.ActivityMainBinding
+import com.example.cryptohub.databinding.ActivityMarketBinding
 import com.example.cryptohub.fragments.ConvertFragment
 import com.example.cryptohub.fragments.HomeFragment
 import com.example.cryptohub.fragments.NewsFragment
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class MarketActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMarketBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMarketBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initBtmNav()
