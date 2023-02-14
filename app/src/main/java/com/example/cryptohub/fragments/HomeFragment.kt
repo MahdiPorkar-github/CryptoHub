abe215193be00af4d9fa9dadd1c56d413367cf21
Mahdi <MahdiPorkar2002@gmail.com>
2023-02-14 05:04:54 -0800

    HomeFragment search functionality added

--- app/src/main/java/com/example/cryptohub/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/cryptohub/fragments/HomeFragment.kt
@@ -3,10 +3,12 @@ package com.example.cryptohub.fragments
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,12 +18,14 @@ import com.example.cryptohub.adapter.CoinsAdapter
 import com.example.cryptohub.databinding.FragmentHomeBinding
 import com.example.cryptohub.model.Coin
 import com.example.cryptohub.model.GetCoinsResponse
+import com.example.cryptohub.model.News
 import com.example.cryptohub.networking.BASE_URL_IMAGE
 import com.example.cryptohub.networking.NetworkChecker
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     lateinit var binding: FragmentHomeBinding
+    lateinit var localData: ArrayList<Coin>
     private val adapter by lazy {
         CoinsAdapter()
     }
@@ -42,16 +46,33 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-            initUi()
-            initListeners()
+        initUi()
+        initListeners()
     }
 
     private fun initListeners() {
+
         binding.layoutWatchlist.btnShowMore.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.livecoinwatch.com/"))
             startActivity(intent)
         }
 
+        binding.edtSearchCoin.addTextChangedListener { input ->
+            searchCoin(input.toString())
+        }
+
+    }
+
+    private fun searchCoin(editTextInput: String) {
+
+        if (editTextInput.isNotEmpty()) {
+            val filteredList = localData.filter { coin ->
+                coin.coinName!!.contains(editTextInput)
+            }
+            adapter.setData(filteredList)
+        } else {
+            adapter.setData(localData)
+        }
     }
 
     private fun initUi() {
@@ -70,10 +91,17 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 val data = arrayListOf<Coin>()
                 coinsResponse.data.forEach {
 
-
-
-                    data.add(Coin(BASE_URL_IMAGE+it.coinInfo.imageUrl,it.coinInfo.fullName, it.dISPLAY.uSD.pRICE, it.rAW.uSD.cHANGEPCT24HOUR, it.rAW.uSD.mKTCAP))
+                    data.add(
+                        Coin(
+                            BASE_URL_IMAGE + it.coinInfo.imageUrl,
+                            it.coinInfo.fullName,
+                            it.dISPLAY.uSD.pRICE,
+                            it.rAW.uSD.cHANGEPCT24HOUR,
+                            it.rAW.uSD.mKTCAP
+                        )
+                    )
                 }
+                localData = data.clone() as ArrayList<Coin>
                 adapter.setData(data)
             }
         }
